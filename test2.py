40a6519b60f3090907ff381abf7922360f8224e2
EXZE1 <oguzhanyucedagg@gmail.com>
2023-04-09 01:42:10 +0300

    ortalama bulma

---
+++ test2.py
@@ -0,0 +1,22 @@
+student_name = None
+student_point = None
+student_list = {}
+
+for i in range(0,3):
+    student_name = input("Öğrencinin adını giriniz ")
+    student_point = int(input("Öğrencinin puanını giriniz "))
+    # list = {student_name:student_point}
+    student_list[student_name] = student_point
+
+print(student_list)
+sum = 0
+for point in student_list.values():
+    print(point)
+    sum = sum + point
+
+ortalama = int(sum/len(student_list))
+print("Sınıf Oratalaması: ", ortalama)
+
+
+
+
